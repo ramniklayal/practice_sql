9868d81a31cb15a13b4d65960a3b0948a4efaebe
Ramnik Layal <ramniklayal@gmail.com>
2023-09-28 13:24:44 -0400

    Added basic datatypes

--- fcc-practice/basic-query.sql
+++ fcc-practice/basic-query.sql
@@ -1,1 +1,8 @@
 INT                     -- Integers (whole numbers)
+DECIMAL(M,N)            -- Decimal data type, First figure in parantheses represents the total number of digits and second figure represents the number of digits after the decimal point
+VARCHAR()               -- String of text with length given in brackets
+BLOB                    -- Stands for Binary Large Object, stores large amounts of binary data, useful for storing images or files
+DATE()                  -- 'YYYY-MM-DD'
+TIMESTAMP()             -- 'YYYY-MM-DD HH:MM:SS'
+-- Above mentioned are some basic datatypes, depends on the database which types are used more
+
