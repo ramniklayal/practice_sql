080c7b82ba8ae52571e32c5ba2d6643bbc677a5f
Ramnik Layal <ramniklayal@gmail.com>
2023-09-30 08:58:35 -0400

    Learned SELECT and ORDER BY sql commands

--- fcc-practice/table-commands.sql
+++ fcc-practice/table-commands.sql
@@ -1,5 +1,5 @@
 CREATE TABLE student (
-    student_id INT,
+    student_id INT PRIMARY KEY,
     name VARCHAR(10),
     major VARCHAR(10)
 );
@@ -11,3 +11,20 @@ DROP TABLE student;
 ALTER TABLE student ADD gpa DECIMAL(3, 2);
 
 ALTER TABLE student DROP COLUMN gpa;
+
+INSERT INTO student VALUES(1, 'Jack', 'Bio');
+INSERT INTO student VALUES(2, 'Kate', 'Sociology');
+INSERT INTO student VALUES(3, 'Claire', 'Chemistry');
+INSERT INTO student VALUES(4, 'Jack', 'Bio' );
+INSERT INTO student VALUES(5, 'Mike', 'Comp Sci');
+
+SELECT * FROM student;
+
+SELECT student.name, student.major  
+FROM student
+ORDER BY name DESC
+LIMIT 3;
+
+SELECT *
+FROM student
+ORDER BY major, student_id;
